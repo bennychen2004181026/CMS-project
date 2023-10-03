878e1cf15d886cf07140477171784e0aaa235172
Ben <ben83026@outlook.com>
2023-10-03 23:19:12 +1100

    Setting up basic server

---
+++ src/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+
+app.use(cors());
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
