21377fd45bfd936bf7129df6636c143fc72e3e69
Ben <ben83026@outlook.com>
2023-10-04 14:39:42 +1100

    Update routes index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
-const {Router} =require('express')
-const studentRouter = require('./student.routes.js')
+const { Router } = require('express');
+const studentRouter = require('./student.routes.js');
+const courseRouter = require('./course.routes.js');
 
-const v1Router = Router()
+const v1Router = Router();
 
-v1Router.use('/students', studentRouter)
+v1Router.use('/students', studentRouter);
+v1Router.use('/courses', courseRouter);
 
-module.exports = v1Router
+module.exports = v1Router;
